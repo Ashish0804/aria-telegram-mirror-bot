b5282957619080f6f72e7451162ef2291e1137e5
Ashish <ashish@pop-os.localdomain>
2021-02-05 18:11:06 +0530

    second commit

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ else
 	echo -e "\nmax-concurrent-downloads=3\nbt-tracker=$tracker_list" >> /bot/aria.conf
 fi
 
-tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',')
 
 aria2c --conf-path=aria.conf --bt-tracker="[$tracker_list]" 
 npm start
